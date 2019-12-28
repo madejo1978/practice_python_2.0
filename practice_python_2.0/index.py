c5eca6e807b89a35ff8085d5dca36d2ad235ebb4
Martinus <madejo@hotmail.com>
2019-12-28 17:49:10 +0100

    v1.1 Strings

--- practice_python_2.0/index.py
+++ practice_python_2.0/index.py
@@ -39,3 +39,6 @@ print("%s, %s" % (g, h))
 # library import: from datetime import datetime
 # datetime.now()
 # extracting / formatting
+from datetime import datetime
+now = datetime.now()
+print("%02d/%02d/%04d" % (now.month, now.day, now.year))
