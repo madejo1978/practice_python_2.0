2132ff02125211447fb501cc2ea2849d809b0af1
Martinus <madejo@hotmail.com>
2019-12-28 17:28:04 +0100

    v1.2 Date and Time

--- practice_python_2.0/README.md
+++ practice_python_2.0/README.md
@@ -2,3 +2,4 @@
 Practice Python 2.0 from Codeacademy
 v1.0 Syntax 
 v1.1 Strings
+v1.2 Date and Time
--- practice_python_2.0/index.py
+++ practice_python_2.0/index.py
@@ -34,3 +34,6 @@ g = "Golf"
 h = "Hotel"
 print("%s, %s" % (g, h))
 
+#v1.2 Date and Time
+ # library import: from datetime import datetime
+ # datetime.now()
