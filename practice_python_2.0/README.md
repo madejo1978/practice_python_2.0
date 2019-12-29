23d0aab24930abddc5d7a661799d4407ec81901b
Martinus <madejo@hotmail.com>
2019-12-29 08:20:17 +0100

    v1.3

--- practice_python_2.0/README.md
+++ practice_python_2.0/README.md
@@ -3,3 +3,4 @@ Practice Python 2.0 from Codeacademy
 v1.0 Syntax 
 v1.1 Strings
 v1.2 Date and Time
+v1.3 Conditionals & Control Flow
--- practice_python_2.0/index.py
+++ practice_python_2.0/index.py
@@ -42,3 +42,7 @@ print("%s, %s" % (g, h))
 from datetime import datetime
 now = datetime.now()
 print("%02d/%02d/%04d" % (now.month, now.day, now.year))
+
+#v1.3 Conditionals & Control Flow
+
+# comparators == != < <= > >=
