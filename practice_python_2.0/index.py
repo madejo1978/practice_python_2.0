7985e293d4e46a685b64d97737cc5410ab03ec20
Martinus <madejo@hotmail.com>
2019-12-31 19:57:47 +0100

    1.4

--- practice_python_2.0/index.py
+++ practice_python_2.0/index.py
@@ -79,3 +79,5 @@ print("%02d/%02d/%04d" % (now.month, now.day, now.year))
  # function(arguments, arguments, etc.) calling a function inputs into a function: arguments
 # generic import. import a module
  # import math => math.sqrt()
+# function import. import a funtion
+ # from math import sqrt
